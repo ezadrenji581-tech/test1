bb9af7d2bb2712ace310bd41de854d1371bbc648
ezadrenji581-tech <ezadrenji581@gmail.com>
2026-04-16 05:27:54 +0800

    Update backend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:5173',           // Untuk testing lokal
-  'https://test1-git-main-ezadrenji581-7673s-projects.vercel.app' // URL Vercel anda
+  'https://test1-zeta-liart-83.vercel.app/' // URL Vercel anda
 ];
 
 app.use(cors({
