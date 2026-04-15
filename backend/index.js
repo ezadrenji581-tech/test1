cd371e5c5aa6737e3c42e187bdd3058dd8269c93
ezadrenji581-tech <ezadrenji581@gmail.com>
2026-04-16 05:32:06 +0800

    Fix CORS trailing slash

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:5173',           // Untuk testing lokal
-  'https://test1-zeta-liart-83.vercel.app/' // URL Vercel anda
+  'https://test1-zeta-liart-83.vercel.app' // URL Vercel anda
 ];
 
 app.use(cors({
